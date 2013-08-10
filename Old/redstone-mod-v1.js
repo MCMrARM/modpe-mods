38b27f0fc311d2f8d6197f9b461b4cb17cc7074c
MCMrsARM <anmail@mail.com>
2013-08-10 18:04:35 +0200

    Updated Redstone Mod

--- Old/redstone-mod-v1.js
+++ Old/redstone-mod-v1.js
@@ -40,30 +40,21 @@ function attackHook(attacker, victim)
 }
 
 var wereThereX, wereThereY, wereThereZ;
-var nextTurn = false;
-var nextEnable = false;
-var nextTurnX, nextTurnY, nextTurnZ, nextTurnLen;
 
 function prepare(){
  wereThereX = new Array();
  wereThereY = new Array();
  wereThereZ = new Array();
- nextTurnX = new Array();
- nextTurnY = new Array();
- nextTurnZ = new Array();
- nextTurnLen = new Array();
 }
 function canGo(x,y,z,mode){
   // MODE 0 - Redstone
   // MODE 1 - Repeator
   // MODE 2 - TNT
   // MODE 3 - Piston
-  // MODE 4 - Door
   if(mode==0?(getTile(x,y,z) == wireId || getTile(x,y,z) == pWireId):
     (mode==1?(getTile(x,y,z) == repeatorId):
     (mode==2?(getTile(x,y,z) == 46):
-    (mode==3?(getTile(x,y,z) == pistonId):
-    (mode==4?(getTile(x,y,z) == 64):false))))){
+    (mode==3?(getTile(x,y,z) == pistonId):false)))){
     for(var i=0;i<wereThereX.length;i++){
       if(wereThereX[i] == x && wereThereY[i] == y && wereThereZ[i] == z){
         // We were there
@@ -75,7 +66,6 @@ function canGo(x,y,z,mode){
     return false;
   }
 }
-
 function goPowerA(enable,x,y,z,len){
   if(canGo(x,y,z,0)){
     // Redstone
@@ -83,27 +73,13 @@ function goPowerA(enable,x,y,z,len){
     wereThereX.push(x);
     wereThereY.push(y);
     wereThereZ.push(z);
-    
-    nextTurn = true;
-    nextEnable = enable;
-    nextTurnX.push(x);
-    nextTurnY.push(y);
-    nextTurnZ.push(z);
-    nextTurnLen.push(len);
-    //goPower(enable,x,y,z,len+1);
+    goPower(enable,x,y,z,len+1);
   }else if(canGo(x,y,z,1)){
     // Repeator
     wereThereX.push(x);
     wereThereY.push(y);
     wereThereZ.push(z);
-    
-    nextTurn = true;
-    nextEnable = enable;
-    nextTurnX.push(x);
-    nextTurnY.push(y);
-    nextTurnZ.push(z);
-    nextTurnLen.push(len);
-    //goPower(enable,x,y,z,0);
+    goPower(enable,x,y,z,0);
   }else if(canGo(x,y,z,2)){
     // TNT
     explode(x,y,z,1);
@@ -115,18 +91,7 @@ function goPowerA(enable,x,y,z,len){
     }else{
       setTile(x,y+1,z,0);
     }
-  }else if(canGo(x,y,z,4)){
-    // Door!
-    if(getTile(x-1,y,z) == 0) { door(x-1,y,z); }
-    else if(getTile(x+1,y,z) == 0) { door(x+1,y,z); }
-    else if(getTile(x,y,z-1) == 0) { door(x,y,z-1); }
-    else if(getTile(x,y,z+1) == 0) { door(x,y,z+1); }
   }
-}
-function door(x, y, z){
- setTile(x,y,z,0);
- setTile(x,y,z,1);
- setTile(x,y,z,0);
 }
 function goPower(enable,x,y,z,len){
   if(len>powerLen) return;
@@ -139,43 +104,25 @@ function goPower(enable,x,y,z,len){
 }
 function modTick()
 {
-  if(nextTurn){
-    nextTurn = false;
-
-    var tntX = nextTurnX; // tnt - tmpNextTurn
-    var tntY = nextTurnY;
-    var tntZ = nextTurnZ;
-    var tntLen = nextTurnLen;
-    nextTurnX = new Array();
-    nextTurnY = new Array();
-    nextTurnZ = new Array();
-    nextTurnLen = new Array();
-    
-    for(var i=0;i<tntX.length;i++){
-      
-      goPower(nextEnable, tntX[i], tntY[i], tntZ[i], tntLen[i]);
-    }
-  }else{
-    if(power){
-      powerTimeout--;
-      if(powerTimeout > 0){
-        if(getTile(powerBlockX, powerBlockY, powerBlockZ) != powererId){
-          prepare();
-          goPower(false, powerBlockX, powerBlockY, powerBlockZ, 0);
-          power = false;
-        }else{
-          rsTick--;
-          if(rsTick <= 0){
-            prepare();
-            goPower(true, powerBlockX, powerBlockY, powerBlockZ, 0);
-            rsTick = redstoneTick;
-          }
-        }
+  if(power){
+  	powerTimeout--;
+    if(powerTimeout > 0){
+      if(getTile(powerBlockX, powerBlockY, powerBlockZ) != powererId){
+      	prepare();
+     	goPower(false, powerBlockX, powerBlockY, powerBlockZ, 0);
+     	power = false;
       }else{
-        prepare();
-        goPower(false, powerBlockX, powerBlockY, powerBlockZ, 0);
-        power = false;
+        rsTick--;
+        if(rsTick <= 0){
+          prepare();
+          goPower(true, powerBlockX, powerBlockY, powerBlockZ, 0);
+          rsTick = redstoneTick;
+        }
       }
+    }else{
+      prepare();
+      goPower(false, powerBlockX, powerBlockY, powerBlockZ, 0);
+      power = false;
     }
   }
 }
