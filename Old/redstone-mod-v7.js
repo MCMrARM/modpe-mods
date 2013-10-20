67690f1e231f93cf36494921ddc71276cbb858c5
MCMrsARM <anmail@mail.com>
2013-10-20 19:24:21 +0200

    Updated Redstone Mod

--- Old/redstone-mod-v7.js
+++ Old/redstone-mod-v7.js
@@ -2,19 +2,16 @@ var powerLen = 14;
 var powerTime = 50;
 var redstoneTick = 20;
 
-var extraData = 9;
-var extraDataWire = 10;
+var buttonId = 255;
+var leverId = 50; // torch, torch, torch. Is there any better solution?
+var repeatorId = 249;
+var wireId = 248;
 var pWireId = 246;
 
-var sButtonId = 255;
-var sLeverId = 85;
-var sRepeatorId = 89;
-var sWireId = 255;
-
-var cButtonId = 16;
-var cLeverId = 56;
-var cRepeatorId = 41;
-var cWireId = 42;
+var aButtonId = 16;
+var aLeverId = 56;
+var aRepeatorId = 41;
+var aWireId = 103;
 
 var pistonId = 128;
 var sPistonId = 156;
@@ -24,21 +21,12 @@ var pressurePlateId = 44;
 
 /* CODE */
 
-var creative = false;
 var power = false;
 var powerTimeout = 0;
 var rsTick = 20;
 var powerBlockX, powerBlockY, powerBlockZ;
 var pistonBlockX, pistonBlockY, pistonBlockZ;
-var buttonId = sButtonId;
-var leverId = sLeverId;
-var repeatorId = sRepeatorId;
-var wireId = sWireId;
 
-ModPE.setItem(55, 3, 8, "Redstone");
-ModPE.setItem(356, 5, 6, "Redstone Repeator");
-ModPE.setItem(77, 15, 7, "Button");
-ModPE.setItem(69, 9, 7, "Lever");
 
 function clearCTable(x,y,z){
   setTile(x-3, y+1, z-2, 0);
@@ -54,74 +42,31 @@ function clearCTable(x,y,z){
   setTile(x-1, y+1, z, 0);
 }
 
-function newLevel(){
- var gamemode = Level.getGameMode();
- if(gamemode == 0){
-  buttonId = sButtonId;
-  leverId = sLeverId;
-  repeatorId = sRepeatorId;
-  wireId = sWireId;
-  creative = false;
- }else if(gamemode == 1){
-  buttonId = cButtonId;
-  leverId = cLeverId;
-  repeatorId = cRepeatorId;
-  wireId = cWireId;
-  creative = true;
- }
-}
-
-function destroyBlock(x,y,z,side){
- var block = getTile(x,y,z);
- var item = getCarriedItem();
- if((Level.getData(x,y,z) == extraDataWire && block == wireId) || (Level.getData(x,y,z) == extraData && block == pWireId)){
-  preventDefault();
-  Level.destroyBlock(x,y,z,false);
-  if(!creative) Level.dropItem(x,y,z,1,55,1,0);
- }else if(Level.getData(x,y,z) == extraData){
-  if(block == 98){
-   preventDefault();
-   Level.destroyBlock(x,y,z,false);
-  }else if(block == repeatorId){
-   preventDefault();
-   Level.destroyBlock(x,y,z,false);
-   if(!creative) Level.dropItem(x,y,z,1,356,1,0);
-  }else if(block == buttonId){
-   preventDefault();
-   Level.destroyBlock(x,y,z,false);
-   if(!creative) Level.dropItem(x,y,z,1,77,1,0);
-  }else if(block == leverId){
-   preventDefault();
-   Level.destroyBlock(x,y,z,false);
-   if(!creative) Level.dropItem(x,y,z,1,69,1,0);
-  }
- }else if((block == 73 || block == 74) && (item == 257 || item == 285 || item == 278)){
-   // if it is redstone ore
-   // drop 4-5 redstone
-   var howMuch = 4+parseInt(Math.random());
-   preventDefault();
-   Level.destroyBlock(x,y,z,false);
-   if(!creative) Level.dropItem(x,y,z,1,55,howMuch,0);
-  }
-}
-
 function useItem(x,y,z,itemId,blockId,side)
 {
-  if(blockId == 58 && itemId == 55 && !creative){
-    if(getTile(x, y, z-1) == 98 && Level.getData(x, y, z-1) == extraData &&
+  if(blockId == aButtonId || blockId == aLeverId || itemId == aWireId){
+    // Use alternative scheme. Okay.
+    buttonId = aButtonId;
+    leverId = aLeverId;
+    repeatorId = aRepeatorId;
+    wireId = aWireId;
+  }
+  
+  if(blockId == 58 && itemId == wireId){
+    if(getTile(x, y, z-1) == 48 &&
        
-       getTile(x-1, y, z) == 98 && Level.getData(x-1, y, z) == extraData &&
-       getTile(x-2, y, z) == 98 && Level.getData(x-2, y, z) == extraData &&
-       getTile(x-3, y, z) == 98 && Level.getData(x-3, y, z) == extraData &&
+       getTile(x-1, y, z) == 48 &&
+       getTile(x-2, y, z) == 48 &&
+       getTile(x-3, y, z) == 48 &&
        
-       getTile(x-1, y, z-1) == 98 && Level.getData(x-1, y, z-1) == extraData &&
-       getTile(x-2, y, z-1) == 98 && Level.getData(x-2, y, z-1) == extraData &&
-       getTile(x-3, y, z-1) == 98 && Level.getData(x-3, y, z-1) == extraData &&
+       getTile(x-1, y, z-1) == 48 &&
+       getTile(x-2, y, z-1) == 48 &&
+       getTile(x-3, y, z-1) == 48 &&
       
              
-       getTile(x-1, y, z-2) == 98 && Level.getData(x-1, y, z-2) == extraData &&
-       getTile(x-2, y, z-2) == 98 && Level.getData(x-2, y, z-2) == extraData &&
-       getTile(x-3, y, z-2) == 98 && Level.getData(x-3, y, z-2) == extraData){
+       getTile(x-1, y, z-2) == 48 &&
+       getTile(x-2, y, z-2) == 48 &&
+       getTile(x-3, y, z-2) == 48){
       
       // craft!
       
@@ -146,7 +91,7 @@ function useItem(x,y,z,itemId,blockId,side)
          c1x2 == 0 && c2x2 == 1 && c3x2 == 0 &&
          c1x3 == 0 && c2x3 == 0 && c3x3 == 0){
         clearCTable(x,y,z);
-        setTile(x, y+1, z-1, buttonId, extraData);
+        setTile(x, y+1, z-1, buttonId);
       }
       
       // Craft lever:
@@ -158,7 +103,7 @@ function useItem(x,y,z,itemId,blockId,side)
          c1x2 == 0 && c2x2 == 5 && c3x2 == 0 &&
          c1x3 == 0 && c2x3 == 1 && c3x3 == 0){
         clearCTable(x,y,z);
-        setTile(x, y+1, z-1, leverId, extraData);
+        setTile(x, y+1, z-1, leverId);
       }
       
       // Craft repeator:
@@ -167,10 +112,10 @@ function useItem(x,y,z,itemId,blockId,side)
       // stone - stone - stone
       
       if(c1x1 == 0 && c2x1 == 0 && c3x1 == 0 &&
-         c1x2 == leverId && c2x2 == (wireId && Level.getData(x-2, y+1, z-1) == extraDataWire) && c3x2 == leverId &&
+         c1x2 == leverId && c2x2 == wireId && c3x2 == leverId &&
          c1x3 == 1 && c2x3 == 1 && c3x3 == 1){
         clearCTable(x,y,z);
-        setTile(x, y+1, z-1, repeatorId, extraData);
+        setTile(x, y+1, z-1, repeatorId);
       }
       
       // Craft piston:
@@ -181,9 +126,9 @@ function useItem(x,y,z,itemId,blockId,side)
       
       if(c1x1 == 5 && c2x1 == 5 && c3x1 == 5 &&
          c1x2 == 4 && c2x2 == 42 && c3x2 == 4 &&
-         c1x3 == 4 && (c2x3 == wireId && Level.getData(x-2, y+1, z) == extraDataWire) && c3x3 == 4){
+         c1x3 == 4 && c2x3 == wireId && c3x3 == 4){
         clearCTable(x,y,z);
-        setTile(x, y+1, z-1, pistonId, extraData);
+        setTile(x, y+1, z-1, pistonId);
       }
       
       // Craft sticky piston:
@@ -196,46 +141,50 @@ function useItem(x,y,z,itemId,blockId,side)
          c1x2 == 0 && c2x2 == 18       && c3x2 == 0 &&
          c1x3 == 0 && c2x3 == pistonId && c3x3 == 0){
         clearCTable(x,y,z);
-        setTile(x, y+1, z-1, sPistonId, extraData);
+        setTile(x, y+1, z-1, pistonId);
       }
       
       preventDefault();
     }else{
       // make a 3x3 square for redstone crafting
-      setTile(x, y-1, z, 98, extraData);
-      setTile(x-1, y-1, z, 98, extraData);
-      setTile(x+1, y-1, z, 98, extraData);
+      // and yes, it doesn't support other items crafting, sorry
+      // and yes, I'm going to create a mod for it
+      setTile(x, y-1, z, 48);
+      setTile(x-1, y-1, z, 48);
+      setTile(x+1, y-1, z, 48);
       
-      setTile(x, y-1, z-1, 98, extraData);
-      setTile(x-1, y-1, z-1, 98, extraData);
-      setTile(x+1, y-1, z-1, 98, extraData);
+      setTile(x, y-1, z-1, 48);
+      setTile(x-1, y-1, z-1, 48);
+      setTile(x+1, y-1, z-1, 48);
       
-      setTile(x, y-1, z+1, 98, extraData);
-      setTile(x-1, y-1, z+1, 98, extraData);
-      setTile(x+1, y-1, z+1, 98, extraData);
+      setTile(x, y-1, z+1, 48);
+      setTile(x-1, y-1, z+1, 48);
+      setTile(x+1, y-1, z+1, 48);
       
       
-      setTile(x+2, y-1, z, 98, extraData);
+      setTile(x+2, y-1, z, 48);
       setTile(x+2, y-1, z+1, 58);
       
       setTile(x, y, z, 0);
       
       preventDefault();
     }
-  }else if(blockId == buttonId && (!creative?(Level.getData(x,y,z) == extraData):true)){
-    powerBlockX = x;
-    powerBlockY = y;
-    powerBlockZ = z
-    ewtX = new Array();
-    ewtY = new Array();
-    ewtZ = new Array();
-    
-    powerTimeout = powerTime;
-    rsTick = 1;
-    power = true;
-    preventDefault();
-  }else if(blockId == leverId && (!creative?(Level.getData(x,y,z) == extraData):true)){
-    powerBlockX = x;
+  }else if(blockId == buttonId){
+    if(buttonId==255?(getTile(x,y-1,z)!=48):true){
+      powerBlockX = x;
+      powerBlockY = y;
+      powerBlockZ = z
+      ewtX = new Array();
+      ewtY = new Array();
+      ewtZ = new Array();
+      
+      powerTimeout = powerTime;
+      rsTick = 1;
+      power = true;
+      preventDefault();
+    }
+  }else if(blockId == leverId){
+   powerBlockX = x;
     powerBlockY = y;
     powerBlockZ = z
     ewtX = new Array();
@@ -246,24 +195,18 @@ function useItem(x,y,z,itemId,blockId,side)
     rsTick = 1;
     power = true;
     preventDefault();
-  }else if(itemId == 55){
-    setBlockWithSide(x,y,z,side,wireId,extraDataWire);
-    addItemInventory(55, -1);
-  }else if(itemId == 356){
-    setBlockWithSide(x,y,z,side,repeatorId,extraData);
-    addItemInventory(356, -1);
-  }else if(itemId == 77){
-    setBlockWithSide(x,y,z,side,buttonId,extraData);
-    addItemInventory(77, -1);
-  }else if(itemId == 69){
-    setBlockWithSide(x,y,z,side,leverId,extraData);
-    addItemInventory(69, -1);
+  }else if((blockId == 73 || blockId == 74) && (itemId == 257 || itemId == 285 || itemId == 278)){
+    // if it is redstone ore
+    // drop 4-5 redstone
+    var howMuch = 4+parseInt(Math.random());
+    addItemInventory(wireId, howMuch);
+    setTile(x,y,z,0);
+    preventDefault();
   }
 }
-
-function setBlockWithSide(x,y,z,side,id,data){
- setTile(x-(side==4?1:0)+(side==5?1:0),y-(side==0?1:0)+(side==1?1:0),z-(side==2?1:0)+(side==3?1:0),id,data);
- 
+function attackHook(attacker, victim)
+{
+//Your Code Here
 }
 
 var wereThereX, wereThereY, wereThereZ;
@@ -287,11 +230,11 @@ function canGo(x,y,z,mode){
   // MODE 2 - TNT
   // MODE 3 - Piston
   // MODE 4 - Door
-  if(mode==0?((getTile(x,y,z) == wireId && (!creative?(Level.getData(x,y,z) == extraDataWire):true)) || (getTile(x,y,z) == pWireId && (!creative?(Level.getData(x,y,z) == extraData):true))):
+  if(mode==0?(getTile(x,y,z) == wireId || getTile(x,y,z) == pWireId):
     (mode==1?(getTile(x,y,z) == repeatorId):
     (mode==2?(getTile(x,y,z) == 46):
     (mode==3?(getTile(x,y,z) == pistonId || getTile(x,y,z) == sPistonId):
-    (mode==4?(getTile(x,y,z) == 64 || getTile(x,y,z) == 71 || getTile(x,y,z) == 96):false))))){
+    (mode==4?(getTile(x,y,z) == 64):false))))){
     for(var i=0;i<wereThereX.length;i++){
       if(wereThereX[i] == x && wereThereY[i] == y && wereThereZ[i] == z){
         // We were there
@@ -315,7 +258,7 @@ function canGo(x,y,z,mode){
 function goPowerA(enable,x,y,z,len,dir){
   if(canGo(x,y,z,0)){
     // Redstone
-    if(enable?(getTile(x,y,z)!=pWireId):(getTile(x,y,z)!=wireId)) setTile(x,y,z,enable?pWireId:wireId,enable?extraData:extraDataWire);
+    if(enable?(getTile(x,y,z)!=pWireId):(getTile(x,y,z)!=wireId)) setTile(x,y,z,enable?pWireId:wireId);
     wereThereX.push(x);
     wereThereY.push(y);
     wereThereZ.push(z);
@@ -386,7 +329,10 @@ pistonDown(x,y,z);
     }
   }else if(canGo(x,y,z,4)){
     // Door!
-    door(x,y,z,enable);
+    if(getTile(x-1,y,z) == 0) { door(x-1,y,z); }
+    else if(getTile(x+1,y,z) == 0) { door(x+1,y,z); }
+    else if(getTile(x,y,z-1) == 0) { door(x,y,z-1); }
+    else if(getTile(x,y,z+1) == 0) { door(x,y,z+1); }
   }
 }
 
@@ -434,12 +380,11 @@ function pistonB(x, y, z, dirX, dirY, dirZ){
   }
 }
 
-function door(x, y, z, enable){
- var open = Level.getData(x,y,z);
- if(open>=4){open=open-4;}
- setTile(x,y,z,getTile(x,y,z),enable?open+4:open);
+function door(x, y, z){
+ setTile(x,y,z,0);
+ setTile(x,y,z,1);
+ setTile(x,y,z,0);
 }
-
 function goPower(enable,x,y,z,len){
   if(len>powerLen) return;
   goPowerA(enable, x-1, y, z, len, 0);
